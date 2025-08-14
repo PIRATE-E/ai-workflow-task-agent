5faacb73923d800b5d341b349b8619cc649db83c
PIRAT-E <piratepiyush579@gmail.com>
2025-08-14 19:32:19 +0530

    fix(utils): remove stale debug_message_protocol import and add compatibility re-exports to new diagnostics path

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,32 @@
-from . import error_transfer
-from . import open_ai_integration
-from . import model_manager
-from . import socket_manager
-from . import structured_triple_prompt
+# Minimal exports; avoid importing deprecated debug modules from old location
+# Provide backward-compatible re-exports to new diagnostics package when available
 
-__all__ = ["error_transfer", "open_ai_integration", "model_manager", "socket_manager", "structured_triple_prompt"]
+from importlib import import_module
+
+__all__ = []
+
+# Backward compatibility: expose debug_helpers & debug_message_protocol from new path
+try:
+    debug_helpers = import_module('src.ui.diagnostics.debug_helpers')
+    __all__.append('debug_helpers')
+except Exception:
+    pass
+
+try:
+    debug_message_protocol = import_module('src.ui.diagnostics.debug_message_protocol')
+    __all__.append('debug_message_protocol')
+except Exception:
+    pass
+
+# Other optional utility modules
+for _mod_name in [
+    'open_ai_integration',
+    'model_manager',
+    'socket_manager',
+    'structured_triple_prompt',
+]:
+    try:
+        globals()[_mod_name] = import_module(f'src.utils.{_mod_name}')
+        __all__.append(_mod_name)
+    except Exception:
+        continue
