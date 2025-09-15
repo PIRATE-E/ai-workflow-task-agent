beb741e4d16090de91500a1a548c027de42149a8
PIRAT-E <piratepiyush579@gmail.com>
2025-09-15 09:05:53 +0530

    fixed - updated init of src to add path of src into sys.path

--- src/__init__.py
+++ src/__init__.py
@@ -7,3 +7,13 @@ Import subpackages explicitly, e.g.:
 
 No implicit re-export of heavy modules to keep startup fast and predictable.
 """
+import os, sys
+
+
+# Ensure parent package (project root) is on sys.path for running as script
+_parent_dir = os.path.dirname(os.path.abspath(__file__))
+if _parent_dir not in sys.path:
+    sys.path.insert(0, _parent_dir)
+
+if __name__ == '__main__':
+    print(_parent_dir)
